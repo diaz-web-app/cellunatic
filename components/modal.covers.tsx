adb2d8b336d0b02ec1378a0d0815c8dbb5c1c855
erickoficial69 <erickoficial69@gmail.com>
2021-09-03 16:14:48 -0400

    aplicando share api

--- components/modal.covers.tsx
+++ components/modal.covers.tsx
@@ -9,7 +9,7 @@ const ModalCovers = ({src,modal,setModal}:Props) => {
     return (
         <>
         <div onClick={()=>setModal(false)} style={{ position: 'fixed', width: '100vw', height: '100vh',top:0,left:0,right:0,bottom:0, zIndex: 1000, background:'var(--primary-color)' }} ></div>
-            <div style={{ padding:'10px 5px', borderRadius:5, position: 'fixed',top:'10%',bottom:'10%',left:'10%',right:'10%',background:'var(--primary-color)', border:'1px solid var(--font-color)', zIndex: 1001 }}>
+            <div className="img_box" style={{ padding:'10px 5px', borderRadius:5, position: 'fixed',background:'var(--primary-color)', border:'1px solid var(--font-color)', zIndex: 1001 }}>
                 <img src={src} alt="cover" />
             </div>
 
@@ -21,12 +21,21 @@ const ModalCovers = ({src,modal,setModal}:Props) => {
                         place-items:center;
                         place-content:center;
                     }
+                    .img_box{
+                        height:height: calc(100vh - 10%);
+                        top:5%;bottom:5%;left:5%;right:5%;
+                    }
                     img{
                         width:100%;
                         height:inherit;
                         object-fit:contain;
                     }
-                    
+                    @media(min-width:960px){
+                        .img_box{
+                            height:height: calc(100vh - 20%);
+                            top:10%;bottom:10%;left:10%;right:10%;
+                        }
+                    }                    
                     `
                 }
             </style>
